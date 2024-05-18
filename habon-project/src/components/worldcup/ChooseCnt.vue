db327d86ad190328fd5ce1cb130167c2377c0a37
yebon <goodyebon1@gmail.com>
2024-05-18 17:59:36 +0900

    월드컵 만드는 중

--- habon-project/src/components/worldcup/ChooseCnt.vue
+++ habon-project/src/components/worldcup/ChooseCnt.vue
@@ -7,16 +7,28 @@
             <option value="8">8강</option>
             <option value="4">4강</option>
         </select>
-        <RouterLink :to="`${$route.path}/${selectedRound}`">라운드 선택</RouterLink>
+        <RouterLink :to="`${$route.path}/${selectedRound}`" @click="initializeRound">라운드 선택</RouterLink>
     </div>
 </template>
 
 <script setup>
 import { RouterLink, useRoute } from 'vue-router';
 import { ref } from 'vue';
+import {useWorldcupStore} from '@/stores/worldcup'
 
 const selectedRound = ref('16'); // 기본값은 16강
 const $route = useRoute(); // 현재 라우터 정보를 가져옴
+
+const store = useWorldcupStore(); //월드컵 피니아 사용
+
+//버튼을 누르는 순간  storeplayList에 월드컵이 저장됩니다!
+const initializeRound = () => {
+
+    // console.log($route.params.id);
+    // console.log(selectedRound.value);
+    
+    store.playWorldcup($route.params.id,selectedRound.value);
+}
 
 </script>
 
