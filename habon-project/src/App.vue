f24ebaa5beb821fc35aa5a22aaad3687999bf1b8
하정수 <kmk2528@naver.com>
2024-05-21 08:57:04 +0900

    하던거 해야지

--- habon-project/src/App.vue
+++ habon-project/src/App.vue
@@ -8,5 +8,24 @@ import TheHeaderNav from '@/components/common/TheHeaderNav.vue'
 </template>
 
 <style scoped>
+  .v-application {
+  font-family: 'BMEuljiro10yearslater', sans-serif !important;
+}
+@font-face {
+  font-family:'BMEuljiro10yearslater';
+  src: url('@/assets/fonts/BMEuljiro10yearslater.ttf') format('truetype');
+  font-weight: 400;
+}
+@font-face {
+  font-family:'BMEuljiro10yearslater';
+  src: url('@/assets/fonts/BMEuljiro10yearslater.ttf') format('truetype');
+  font-weight: 300;
+}
+@font-face {
+  font-family:'BMEuljiro10yearslater';
+  src: url('@/assets/fonts/BMEuljiro10yearslater.ttf') format('truetype');
+  font-weight: 700;
+}
+
 
 </style>
